d485427ed959f51e6f38331fb7c536c2c2f7dde6
Hayden Bleasel <hello@haydenbleasel.com>
2024-06-25 06:49:45 -0400

    Update navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -7,9 +7,11 @@ import type { FC } from 'react';
 export const Navbar: FC = () => (
   <nav className="border-t-4 border-primary">
     <div className="px-6 py-4 flex items-center justify-between">
-      <div className="flex items-center gap-2">
+      <div className="flex items-center gap-1.5">
         <Logo />
-        <p>Code Explorer</p>
+        <p className="text-xl font-semibold text-muted-foreground translate-y-px">
+          Code Explorer
+        </p>
       </div>
       <div className="flex items-center gap-2">
         <ModeToggle />
