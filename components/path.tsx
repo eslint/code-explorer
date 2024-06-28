28d64a2c3219488ca377414928c8988f051db28f
Hayden Bleasel <hello@haydenbleasel.com>
2024-06-28 06:36:04 -0400

    Start working on code path

--- components/path.tsx
+++ components/path.tsx
@@ -1,10 +1,30 @@
 'use client';
 
+import { useEffect, useState } from 'react';
 import { useExplorer } from '@/hooks/use-explorer';
+import { generateCodePath } from '@/app/actions/generate-code-path';
+import { Editor } from './editor';
 import type { FC } from 'react';
 
 export const CodePath: FC = () => {
   const explorer = useExplorer();
+  const [extracted, setExtracted] = useState(null);
 
-  return <p>Code Path</p>;
+  useEffect(() => {
+    generateCodePath(explorer.code, explorer.esVersion, explorer.sourceType)
+      .then(setExtracted)
+      .catch(console.error);
+  }, [explorer.code, explorer.esVersion, explorer.sourceType]);
+
+  console.log(extracted);
+
+  if (!extracted) {
+    return <pre>Loading...</pre>;
+  }
+
+  if (extracted.error) {
+    return <pre>{extracted.error}</pre>;
+  }
+
+  return <Editor value={extracted.response} />;
 };
