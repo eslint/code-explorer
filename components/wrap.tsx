19dd7c6fcef57ca3e24aa63b7de888fff9eba240
Hayden Bleasel <hello@haydenbleasel.com>
2024-07-03 11:37:01 -0400

    Hide Wrap button on non-code views

--- components/wrap.tsx
+++ components/wrap.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { WrapTextIcon } from 'lucide-react';
+import { usePathname } from 'next/navigation';
 import { useExplorer } from '@/hooks/use-explorer';
 import { cn } from '@/lib/utils';
 import { Button } from './ui/button';
@@ -8,6 +9,15 @@ import type { FC } from 'react';
 
 export const Wrap: FC = () => {
   const explorer = useExplorer();
+  const pathname = usePathname();
+
+  if (pathname === '/' && explorer.astViewMode !== 'json') {
+    return null;
+  }
+
+  if (pathname === '/path' && explorer.pathViewMode !== 'code') {
+    return null;
+  }
 
   return (
     <Button
