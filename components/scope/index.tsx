1bceb4afb31624a26a1679eee69c1fee8bb07e9d
Hayden Bleasel <hello@haydenbleasel.com>
2024-07-12 00:11:57 -0400

    Fix default value of scope

--- components/scope/index.tsx
+++ components/scope/index.tsx
@@ -29,7 +29,11 @@ export const Scope: FC = () => {
   });
 
   return (
-    <Accordion type="multiple" className="px-8 font-mono space-y-3">
+    <Accordion
+      type="multiple"
+      className="px-8 font-mono space-y-3"
+      defaultValue={['0-global']}
+    >
       {explorer.scopeViewMode === 'flat' ? (
         <>
           {scopeManager.scopes.map((subScope, index) => (
