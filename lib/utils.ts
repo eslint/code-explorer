71b6a3a6c1faac83f715525fe8cbebc30c558c07
Hayden Bleasel <hello@haydenbleasel.com>
2024-07-18 12:40:39 -0400

    Temporarily remove all files

--- lib/utils.ts
+++
@@ -1,8 +0,0 @@
-import { clsx } from 'clsx';
-import { twMerge } from 'tailwind-merge';
-import type { ClassValue } from 'clsx';
-
-export const cn = (...inputs: ClassValue[]): string => twMerge(clsx(inputs));
-
-export const capitalize = (substring: string): string =>
-  substring.charAt(0).toUpperCase() + substring.slice(1);
