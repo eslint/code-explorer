f405c9e0c31b56ae275a99ea16eaef6f2c9f01b7
Strek <ssharishkumar@gmail.com>
2024-07-26 14:42:23 +0530

    fix: initial load error

--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Editor as MonacoEditor, useMonaco } from '@monaco-editor/react';
+import { Editor as MonacoEditor } from '@monaco-editor/react';
 import { useExplorer } from '@/hooks/use-explorer';
 import type { ComponentProps, FC } from 'react';
 
@@ -8,30 +8,30 @@ type EditorProperties = ComponentProps<typeof MonacoEditor>;
 
 export const Editor: FC<EditorProperties> = (properties) => {
   const { theme = "system" } = useExplorer();
-  const monaco = useMonaco();
   const explorer = useExplorer();
-
-  monaco?.editor.defineTheme('eslint-light', {
-    base: 'vs',
-    inherit: true,
-    rules: [],
-    colors: {
-      'editor.background': '#FFFFFF00',
-    },
-  });
-
-  monaco?.editor.defineTheme('eslint-dark', {
-    base: 'vs-dark',
-    inherit: true,
-    rules: [],
-    colors: {
-      'editor.background': '#FFFFFF00',
-    },
-  });
 
   return (
     <MonacoEditor
       height="100%"
+      onMount={(_editor, monaco) => {
+        monaco?.editor.defineTheme("eslint-light", {
+          base: "vs",
+          inherit: true,
+          rules: [],
+          colors: {
+            "editor.background": "#FFFFFF00",
+          },
+        });
+
+        monaco?.editor.defineTheme("eslint-dark", {
+          base: "vs-dark",
+          inherit: true,
+          rules: [],
+          colors: {
+            "editor.background": "#FFFFFF00",
+          },
+        });
+      }}
       options={{
         minimap: {
           enabled: false,
