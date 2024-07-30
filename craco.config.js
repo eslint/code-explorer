f5d5e9b52f7fc091a99fe220c534c3285f292f2f
Strek <ssharishkumar@gmail.com>
2024-07-30 18:17:44 +0530

    chore: add terser to optimization instead of plugins array

--- craco.config.js
+++ craco.config.js
@@ -1,27 +1,26 @@
-const TerserPlugin = require('terser-webpack-plugin');
+const TerserPlugin = require("terser-webpack-plugin");
 
-const path = require('path');
+const path = require("path");
 module.exports = {
   webpack: {
-    plugins: {
-      add: [
+    configure: (webpackConfig) => {
+      webpackConfig.resolve.fallback = {
+        ...webpackConfig.resolve.fallback,
+        assert: require.resolve("assert/"),
+      };
+
+      webpackConfig.optimization.minimizer = [
         new TerserPlugin({
           terserOptions: {
             keep_fnames: true,
           },
         }),
-      ],
-    },
-    configure: (webpackConfig) => {
-      webpackConfig.resolve.fallback = {
-        ...webpackConfig.resolve.fallback,
-        "assert": require.resolve("assert/"),
-      };
+      ];
 
       return webpackConfig;
     },
     alias: {
-      '@': path.resolve(__dirname, 'src'),
+      "@": path.resolve(__dirname, "src"),
     },
   },
 };
