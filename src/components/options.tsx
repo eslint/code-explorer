eslint/code-explorer

commit 7c1a82f9c27efec9b5b54b6260ae990b8125b79a

feat: Add gear next to language in header

src/components/options.tsx
@@ -26,6 +26,8 @@ import {
 import { Button } from './ui/button';
 import { Label } from './ui/label';
 import type { FC } from 'react';
+import { Settings } from 'lucide-react';
+
 
 export const Options: FC = () => {
   const explorer = useExplorer();
@@ -58,6 +60,7 @@ export const Options: FC = () => {
             className="w-4 h-4"
           />
           <span>{currentLanguage.label}</span>
+          <Settings className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
         </Button>
       </PopoverTrigger>
       <PopoverContent className="space-y-4 w-[372px]">

commit 69165b6816acd9444166024ec342cc9ff6a2f636

revert add gear icon commit
Revert add gear icon commit - 7c1a82f9c27efec9b5b54b6260ae990b8125b79a
Accidentally committed the changes to main.

src/components/options.tsx
@@ -26,7 +26,6 @@ import {
 import { Button } from './ui/button';
 import { Label } from './ui/label';
 import type { FC } from 'react';
-import { Settings } from 'lucide-react';
 
 
 export const Options: FC = () => {
@@ -60,7 +59,6 @@ export const Options: FC = () => {
             className="w-4 h-4"
           />
           <span>{currentLanguage.label}</span>
-          <Settings className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
         </Button>
       </PopoverTrigger>
       <PopoverContent className="space-y-4 w-[372px]">