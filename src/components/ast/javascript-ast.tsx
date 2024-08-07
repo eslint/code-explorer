97b91d21f6ef531df6730255aa5db62dc62fcdd5
Amaresh  S M <amareshsm13@gmail.com>
2024-08-07 20:33:42 +0530

    fix: Make AST tree view node as expanded by default (#17)

--- src/components/ast/javascript-ast.tsx
+++ src/components/ast/javascript-ast.tsx
@@ -38,7 +38,7 @@ export const JavascriptAst: FC = () => {
         className="px-8 font-mono space-y-3"
         defaultValue={['0-Program']}
       >
-        <JavascriptAstTreeItem data={tree} index={-1} />
+        <JavascriptAstTreeItem data={tree} index={0} />
       </Accordion>
     );
   }
