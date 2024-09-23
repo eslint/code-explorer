1d3d7bd7e589da7b62c1cc2f6432e84e10e94d42
Amaresh  S M <30730124+amareshsm@users.noreply.github.com>
2024-09-24 03:23:00 +0530

    fix: show type for object similar to array

--- src/lib/render-value.ts
+++ src/lib/render-value.ts
@@ -1,6 +1,27 @@
 export const renderValue = (value: unknown): string[] => {
 	if (Array.isArray(value)) {
-		return ["Array", `[${value.length} elements]`];
+		return [
+			"Array",
+			value.length
+				? value.length === 1
+					? `[${value.length} element]`
+					: `[${value.length} elements]`
+				: "[]",
+		];
+	}
+
+	if (value instanceof Object && Object.keys(value).length === 0) {
+		return ["Object", "{}"];
+	}
+
+	if (value instanceof Object && !(value as { type: string }).type) {
+		const keys = Object.keys(value);
+		return [
+			"Object",
+			keys.length > 3
+				? `{${keys.slice(0, 3).join(", ")}, ...}`
+				: `{${keys.join(", ")}}`,
+		];
 	}
 
 	if (typeof value === "object" && value !== null) {
