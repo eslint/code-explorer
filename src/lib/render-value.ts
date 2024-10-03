24d7786ac31dd93ce719bf76db0c00d15bedddd6
Amaresh  S M <amareshsm13@gmail.com>
2024-10-03 19:13:12 +0530

    fix: minor glitches in the right pane (#58)
    Co-authored-by: Strek <ssharishkumar@gmail.com>
    Co-authored-by: Nicholas C. Zakas <nicholas@humanwhocodes.com>

--- src/lib/render-value.ts
+++ src/lib/render-value.ts
@@ -1,12 +1,23 @@
 export const renderValue = (value: unknown): string[] => {
 	if (Array.isArray(value)) {
-		return ["Array", `[${value.length} elements]`];
+		return [
+			"Array",
+			value.length
+				? value.length === 1
+					? `[${value.length} element]`
+					: `[${value.length} elements]`
+				: "[]",
+		];
+	}
+
+	if (value instanceof Object && Object.keys(value).length === 0) {
+		return ["Object", "{}"];
 	}
 
 	if (typeof value === "object" && value !== null) {
 		const keys = Object.keys(value);
 		return [
-			(value as { type: string })?.type,
+			(value as { type: string })?.type ?? "Object",
 			keys.length > 3
 				? `{${keys.slice(0, 3).join(", ")}, ...}`
 				: `{${keys.join(", ")}}`,
