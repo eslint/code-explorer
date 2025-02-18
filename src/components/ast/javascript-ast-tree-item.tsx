3460511e08b1757efe1d94661e8c3c21c63ea459
Patrick Kerschbaum <patrick.kerschbaum@gmail.com>
2025-02-18 20:08:05 +0100

    fix: also highlight root JS AST node (`Program`) if matched by ESQuery selector

--- src/components/ast/javascript-ast-tree-item.tsx
+++ src/components/ast/javascript-ast-tree-item.tsx
@@ -6,6 +6,7 @@ import {
 import { TreeEntry } from "../tree-entry";
 import type { FC } from "react";
 import type * as espree from "espree";
+import { cn } from "@/lib/utils";
 
 export type JavascriptAstTreeItemProperties = {
 	readonly index: number;
@@ -19,24 +20,31 @@ export const JavascriptAstTreeItem: FC<JavascriptAstTreeItemProperties> = ({
 	data,
 	index,
 	esqueryMatchedNodes,
-}) => (
-	<AccordionItem
-		value={`${index}-${data.type}`}
-		className="border border-card rounded-lg overflow-hidden"
-	>
-		<AccordionTrigger className="text-sm bg-card px-4 py-3 capitalize">
-			{data.type}
-		</AccordionTrigger>
-		<AccordionContent className="p-4 border-t">
-			<div className="space-y-1">
-				{Object.entries(data).map(item => (
-					<TreeEntry
-						key={item[0]}
-						data={item}
-						esqueryMatchedNodes={esqueryMatchedNodes}
-					/>
-				))}
-			</div>
-		</AccordionContent>
-	</AccordionItem>
-);
+}) => {
+	const isEsqueryMatchedNode = esqueryMatchedNodes.includes(data);
+
+	return (
+		<AccordionItem
+			value={`${index}-${data.type}`}
+			className={cn(
+				"border border-card rounded-lg overflow-hidden",
+				isEsqueryMatchedNode && "border-primary border-4",
+			)}
+		>
+			<AccordionTrigger className="text-sm bg-card px-4 py-3 capitalize">
+				{data.type}
+			</AccordionTrigger>
+			<AccordionContent className="p-4 border-t">
+				<div className="space-y-1">
+					{Object.entries(data).map(item => (
+						<TreeEntry
+							key={item[0]}
+							data={item}
+							esqueryMatchedNodes={esqueryMatchedNodes}
+						/>
+					))}
+				</div>
+			</AccordionContent>
+		</AccordionItem>
+	);
+};
