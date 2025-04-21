5917850ed6a0d64e8c88f9bf0622ecc905fad1d0
xbinaryx <xbinaryx68@gmail.com>
2025-04-21 18:44:06 +0300

    refactor: remove accordion behavior for non-array nodes (#91)

--- src/components/scope/scope-item.tsx
+++ src/components/scope/scope-item.tsx
@@ -46,18 +46,43 @@ export const ScopeItem: FC<ScopeItemProperties> = ({
 		([name]) => !name.startsWith("__"),
 	);
 
+	if (isArray) {
+		return (
+			<AccordionItem
+				value={path + "." + index + "." + key}
+				className={cn(
+					"border border-card rounded-lg overflow-hidden",
+					isEsqueryMatchedNode && "border-primary border-4",
+				)}
+			>
+				<AccordionTrigger className="text-sm bg-card px-4 py-3 capitalize">
+					{`${Math.max(index, 0)}. ${key}`}
+				</AccordionTrigger>
+				<AccordionContent className="p-4 border-t">
+					<div className="space-y-1">
+						{properties.map((item, index) => (
+							<TreeEntry
+								key={item[0]}
+								data={item}
+								path={path + "." + index}
+								esqueryMatchedNodes={esqueryMatchedNodes}
+							/>
+						))}
+					</div>
+				</AccordionContent>
+			</AccordionItem>
+		);
+	}
+
 	return (
-		<AccordionItem
-			value={path + "." + index + "." + key}
+		<div
 			className={cn(
 				"border border-card rounded-lg overflow-hidden",
 				isEsqueryMatchedNode && "border-primary border-4",
 			)}
 		>
-			<AccordionTrigger className="text-sm bg-card px-4 py-3 capitalize">
-				{isArray && `${Math.max(index, 0)}.`} {key}
-			</AccordionTrigger>
-			<AccordionContent className="p-4 border-t">
+			<h3 className="text-sm bg-card px-4 py-3 capitalize">{key}</h3>
+			<div className="p-4 border-t">
 				<div className="space-y-1">
 					{properties.map((item, index) => (
 						<TreeEntry
@@ -68,7 +93,7 @@ export const ScopeItem: FC<ScopeItemProperties> = ({
 						/>
 					))}
 				</div>
-			</AccordionContent>
-		</AccordionItem>
+			</div>
+		</div>
 	);
 };
