e1186bce0cd49e7284ed5a5df8086f327dceaf7d
루밀LuMir <rpfos@naver.com>
2025-10-11 22:25:57 +0900

    refactor: bump `@types/eslint-scope` to latest (#181)

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 
 const root = ReactDOM.createRoot(
-	document.getElementById("root") as HTMLElement,
+	document.getElementById("root") as HTMLDivElement,
 );
 root.render(
 	<React.StrictMode>
