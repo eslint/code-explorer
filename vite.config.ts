4e7782d59123e577269d4341107bc94f547e2548
루밀LuMir <rpfos@naver.com>
2025-12-07 14:22:40 +0900

    chore: update `@html-eslint/eslint-plugin`

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
+import path from "node:path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 
 export default defineConfig({
 	plugins: [
