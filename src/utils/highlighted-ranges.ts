7a29ab742653c0ba89b1286bcd1cd132ef8fd3bf
Pixel998 <mrhbs998@gmail.com>
2026-03-25 18:54:06 +0300

    fix: keep ESQuery highlights aligned while typing

--- src/utils/highlighted-ranges.ts
+++ src/utils/highlighted-ranges.ts
@@ -1,17 +1,31 @@
-import { Decoration, ViewPlugin } from "@codemirror/view";
+import { Decoration, ViewPlugin, type ViewUpdate } from "@codemirror/view";
 import type { SourceRange } from "@eslint/core";
 
 const highlightRangeDecoration = Decoration.mark({
 	class: "bg-editorHighlightedRangeColor",
 });
 
+function createHighlightedRangeDecorations(ranges: SourceRange[]) {
+	return Decoration.set(
+		ranges.map(([rangeFrom, rangeTo]) =>
+			highlightRangeDecoration.range(rangeFrom, rangeTo),
+		),
+		true,
+	);
+}
+
 export const highlightedRangesExtension = (ranges: SourceRange[]) =>
-	ViewPlugin.define(() => ({}), {
-		decorations: () =>
-			Decoration.set(
-				ranges.map(([rangeFrom, rangeTo]) =>
-					highlightRangeDecoration.range(rangeFrom, rangeTo),
-				),
-				true,
-			),
-	});
+	ViewPlugin.define(
+		() => ({
+			decorations: createHighlightedRangeDecorations(ranges),
+
+			update(update: ViewUpdate) {
+				if (update.docChanged) {
+					this.decorations = this.decorations.map(update.changes);
+				}
+			},
+		}),
+		{
+			decorations: value => value.decorations,
+		},
+	);
